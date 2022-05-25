5c749c2d40c09ef97d5881f76f495514de3c4f67
uyenjune795 <80790327+uyenjune795@users.noreply.github.com>
2022-05-25 00:51:19 -0700

    Added files

---
+++ README.md
@@ -0,0 +1,4 @@
+# CircleText with Confetti effects
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/uyenjune795/pen/JjpNRVe](https://codepen.io/uyenjune795/pen/JjpNRVe).
+
---
+++ script.js
@@ -0,0 +1,17 @@
+//instantiate new object
+let confetti = new Confetti ('button-container');
+    /*variable*/ /*library*/
+
+//options
+confetti.destroyTarget(false);
+confetti.setSize(1.5);
+
+import CircleType from "https://cdn.skypack.dev/circletype@2.3.0";
+
+// Instantiate `CircleType` with an HTML element.
+//const circleType = new CircleType(document.getElementById('text'));
+
+// Set the text radius and direction. Note: setter methods are chainable.
+//circleType.radius(100).dir(1);
+
+const circleType = new CircleType(document.getElementById('text'));
